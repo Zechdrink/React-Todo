516c6344984dde85fa1fd1aea8581e5aa4b9075f
Zech Drinkhall <zechdrink@aol.com>
2019-02-13 15:51:38 -0800

    the form adds new items but doesnt delete them yet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from 'react';
 
+import './components/TodoComponents/Todo.css';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+const toDoArr  = [
+{
+
+}
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  //constructor
+  constructor(){
+    super();
+    this.state = {
+      stuff: toDoArr,
+      inputText: '',
+      id: '',
+      completed: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  addListItem = event => {
+    event.preventDefault();
+    this.setState({
+      stuff: [...this.state.stuff, {task: this.state.inputText}]
+    })  
+  }
+
+  
+  //render is a lifecycle method => returns our jsx
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className = "App">
+        <TodoList wholeList = {this.state.stuff} />
+        <TodoForm 
+          addListItem = {this.addListItem}
+          inputText = {this.state.inputText} 
+          handleChange = {this.handleChange}
+          />
       </div>
     );
   }
 }
 
 export default App;
+
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
