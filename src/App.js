0d2b7c9fe10571d6721a3dc17df36fe687012e3e
Zech Drinkhall <zechdrink@aol.com>
2019-02-14 13:56:30 -0800

    created components for todo and todolist and imported them in their respective places.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import './App.css';
+
+import TodoList from './components/TodoComponents/TodoList';
+
+
+
+const todos = [{
+  task: 'eat',
+  id: 33,
+  complete: false
+},
+{
+  task: 'watch anime',
+  id: 34,
+  complete: false
+}];
 
 class App extends React.Component {
   //constructor
- 
+  constructor(){
+    super();
+    this.state = {
+      todos: todos,
+    } 
+
+  }
   //render is a lifecycle method => returns our jsx
   render() {
     return (
       <div className = "App">
-          Testing.
+          <h1>Stuff To Do</h1>
+          <TodoList todos = {this.state.todos} />
       </div>
     );
   }
